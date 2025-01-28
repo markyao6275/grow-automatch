a0ed42ef451c899c5afd27fa19707e3e9595d173
Alexander <ajoxales@gmail.com>
2025-01-28 13:16:19 +0800

    [GRO-14] Build Simple UI for Python Program

--- script.py
+++ script.py
@@ -1,14 +1,15 @@
+import sys
 import pandas as pd
-
+from PyQt6.QtWidgets import QApplication
 from process_resumes import process_resumes
 from process_job_descriptions import process_job_descriptions
 from score_candidates import score_candidates
+from display_ui import DisplayUI
 
 folder_containing_resumes = "./resumes"
 folder_containing_job_descriptions = "./job_descriptions"
 
-
-def main():
+def start_processing():
     processed_resumes_file = process_resumes(folder_containing_resumes)
     if not processed_resumes_file:
         print("No resumes processed")
@@ -23,19 +24,20 @@ def main():
     # Read the CSV file
     df = pd.read_csv(
         processed_job_descriptions_file
-        # "./output/POMVOM_JD.csv" # For testing only
     )
 
     # Iterate through each job description
     for index, row in df.iterrows():
-        if index == 0:  # For testing, limit to first job description
-            job_data = row.to_dict()
-            score_candidates(
-                job_data,
-                processed_resumes_file,
-                # "output/perfect_candidates_pomvom.csv", # For testing only
-            )
-
-
+        job_data = row.to_dict()
+        score_candidates(
+            job_data,
+            processed_resumes_file,
+        )
+    
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+
+    window = DisplayUI(start_processing)
+    window.show()
+
+    sys.exit(app.exec())
