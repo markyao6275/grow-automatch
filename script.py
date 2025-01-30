13b58c90150fc8f568704bf9cb50e601db8ae982
Alexander <ajoxales@gmail.com>
2025-01-30 11:52:49 +0800

    Re-added code of start_processing function

--- script.py
+++ script.py
@@ -14,25 +14,24 @@ candidates_to_score = [0] # If 0, score perfect and strong candidates.
 
 def start_processing():
     print("Candidates to Score:", candidates_to_score[0])
-    
-    # processed_resumes_file = process_resumes(folder_containing_resumes)
-    # if not processed_resumes_file:
-    #     print("No resumes processed")
-    #     return
-    # processed_job_descriptions_file = process_job_descriptions(
-    #     folder_containing_job_descriptions
-    # )
-    # if not processed_job_descriptions_file:
-    #     print("No job descriptions processed")
-    #     return
+    processed_resumes_file = process_resumes(folder_containing_resumes)
+    if not processed_resumes_file:
+        print("No resumes processed")
+        return
+    processed_job_descriptions_file = process_job_descriptions(
+        folder_containing_job_descriptions
+    )
+    if not processed_job_descriptions_file:
+        print("No job descriptions processed")
+        return
 
-    # # Read the CSV file
-    # df = pd.read_csv(processed_job_descriptions_file)
+    # Read the CSV file
+    df = pd.read_csv(processed_job_descriptions_file)
 
-    # # Iterate through each job description
-    # for index, row in df.iterrows():
-    #     job_data = row.to_dict()
-    #     score_candidates(job_data, processed_resumes_file)
+    # Iterate through each job description
+    for index, row in df.iterrows():
+        job_data = row.to_dict()
+        score_candidates(job_data, processed_resumes_file)
 
 
 if __name__ == "__main__":
