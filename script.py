4b52c066c87934dbf35b21429af30c08978ec029
Alexander <ajoxales@gmail.com>
2025-01-30 11:32:20 +0800

    [GRO-21] Add Input Score Top Button

--- script.py
+++ script.py
@@ -10,33 +10,36 @@ from display_ui import DisplayUI
 folder_containing_resumes = "./resumes"
 folder_containing_job_descriptions = "./job_descriptions"
 
+candidates_to_score = [0] # If 0, score perfect and strong candidates.
 
 def start_processing():
-    processed_resumes_file = process_resumes(folder_containing_resumes)
-    if not processed_resumes_file:
-        print("No resumes processed")
-        return
-    processed_job_descriptions_file = process_job_descriptions(
-        folder_containing_job_descriptions
-    )
-    if not processed_job_descriptions_file:
-        print("No job descriptions processed")
-        return
+    print("Candidates to Score:", candidates_to_score[0])
+    
+    # processed_resumes_file = process_resumes(folder_containing_resumes)
+    # if not processed_resumes_file:
+    #     print("No resumes processed")
+    #     return
+    # processed_job_descriptions_file = process_job_descriptions(
+    #     folder_containing_job_descriptions
+    # )
+    # if not processed_job_descriptions_file:
+    #     print("No job descriptions processed")
+    #     return
 
-    # Read the CSV file
-    df = pd.read_csv(processed_job_descriptions_file)
+    # # Read the CSV file
+    # df = pd.read_csv(processed_job_descriptions_file)
 
-    # Iterate through each job description
-    for index, row in df.iterrows():
-        job_data = row.to_dict()
-        score_candidates(job_data, processed_resumes_file)
+    # # Iterate through each job description
+    # for index, row in df.iterrows():
+    #     job_data = row.to_dict()
+    #     score_candidates(job_data, processed_resumes_file)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     # Create the DisplayUI instance and pass the start_processing function
-    window = DisplayUI(start_processing)
+    window = DisplayUI(start_processing, candidates_to_score)
     window.show()
 
     sys.exit(app.exec())
