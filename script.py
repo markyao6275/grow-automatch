a62c6fc995a3784ff2b9a8f597e64fb1384ae2a5
Jay Mercado <jaymercado@Jays-MacBook-Pro.local>
2025-02-04 16:11:36 +0800

    Fix script.py

--- script.py
+++ script.py
@@ -12,24 +12,24 @@ folder_containing_job_descriptions = "./job_descriptions"
 
 
 def start_processing():
-    # processed_resumes_file = process_resumes(folder_containing_resumes)
-    # if not processed_resumes_file:
-    #     print("No resumes processed")
-    #     return
-    # processed_job_descriptions_file = process_job_descriptions(
-    #     folder_containing_job_descriptions
-    # )
-    # if not processed_job_descriptions_file:
-    #     print("No job descriptions processed")
-    #     return
+    processed_resumes_file = process_resumes(folder_containing_resumes)
+    if not processed_resumes_file:
+        print("No resumes processed")
+        return
+    processed_job_descriptions_file = process_job_descriptions(
+        folder_containing_job_descriptions
+    )
+    if not processed_job_descriptions_file:
+        print("No job descriptions processed")
+        return
 
     # Read the CSV file
-    df = pd.read_csv("./output/job_descriptions_20250203_162918.csv")
+    df = pd.read_csv(processed_job_descriptions_file)
 
     # Iterate through each job description
     for index, row in df.iterrows():
         job_data = row.to_dict()
-        score_candidates(job_data, "./output/resumes_20250203_162746.csv")
+        score_candidates(job_data, processed_resumes_file)
 
 
 if __name__ == "__main__":
